5bef49633a546248808fe0546229d735bfd516f5
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2020-12-23 11:02:51 +0530

    Added Blog

--- src/Blogs.js
+++ src/Blogs.js
@@ -22,9 +22,60 @@ const useStyles = makeStyles((theme) => ({
   }));
 function Blogs() {
   const classes = useStyles();
+  const blogObject=[
+    {
+      name:"Develop a Simple Progressive Web App with React",
+      completed:"December 2020",
+      description:"Web applications can reach anyone, anywhere, on any device with a single codebase.This article provides a step-by-step demonstration to convert your React application into an installable PWA",
+      blogUrl:"https://eshaansinghparihar.medium.com/develop-a-simple-progressive-web-app-with-react-b5d8272c3102",
+      githubUrl:"https://github.com/eshaansinghparihar/simple-pwa",
+    },
+  ]
+
+  const blogCard = blogObject.map(blog=>{
+    return(
+      <Paper
+        item
+        alignContent="center"
+        spacing={2}
+        elevation={8}
+        className={classes.card}
+      >
+        <CardContent>
+          <Typography variant="subtitle1" component="h4">
+          <h3>{blog.name}</h3>
+          </Typography>
+          <h5>Completed in {blog.completed}</h5>
+          <Typography variant="subtitle2" component="h4">
+            <p>{blog.description}</p>
+            </Typography>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            className={classes.submit}
+          >
+          <a target="_blank" href={blog.blogUrl} rel="noreferrer">Read Blog</a>
+          </Button>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+          <a target="_blank" href={blog.githubUrl} rel="noreferrer">{blog.githubUrl?"View Github Repo":"Git Repo NA"}</a>
+          </Button>
+        </CardContent>
+      </Paper>
+    );
+  })
+
   return (
     <div className="App">
         <CssBaseline />
+        {blogCard}
         <Paper
           item
           alignContent="center"
@@ -58,40 +109,6 @@ function Blogs() {
               className={classes.submit}
             >
             <a target="_blank" href="https://github.com/eshaansinghparihar/selfhelp">View Github Repo</a>
-            </Button>
-          </CardContent>
-        </Paper>
-        <Paper
-          item
-          alignContent="center"
-          spacing={2}
-          elevation={8}
-          className={classes.card}
-        >
-          <CardContent>
-            <Typography variant="subtitle1" component="h4">
-              <h3>Technical Blogs</h3>
-            </Typography>
-            <Typography variant="subtitle2" component="h4">
-            <p>Training to write Technical Blogs in action.</p>
-            </Typography>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="secondary"
-              className={classes.submit}
-            >
-            <a target="_blank" href="/">Coming Soon</a>
-            </Button>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-            <a target="_blank" href="/">Coming Soon</a>
             </Button>
           </CardContent>
         </Paper>
